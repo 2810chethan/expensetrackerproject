7feff77012fcba2e1b1ac81310a3a85430c3bd70
chethankalyan <chethankalyan664@gmail.com>
2026-05-20 12:20:29 +0530

    fix register navigation

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
+  const navigate = useNavigate();
 
   const [user, setUser] = useState({
     name: "",
@@ -17,56 +19,38 @@ function Register() {
   };
 
   const handleRegister = async () => {
-
     try {
-
       await axios.post(
-        "https://expensetrackerproject-puzd.onrender.com/api/auth/register",
+        "https://https://expensetrackerproject-puzd.onrender.com/api/auth/register",
         user
       );
 
-      alert("User Registered");
-
+      alert("User Registered Successfully");
+      navigate("/");
     } catch (err) {
+      alert("Registration Failed");
       console.log(err);
     }
-
   };
 
   return (
     <div style={{ padding: "30px" }}>
       <h1>Register</h1>
 
-      <input
-        type="text"
-        name="name"
-        placeholder="Name"
-        onChange={handleChange}
-      />
+      <input type="text" name="name" placeholder="Name" onChange={handleChange} />
+      <br /><br />
+
+      <input type="email" name="email" placeholder="Email" onChange={handleChange} />
+      <br /><br />
+
+      <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+      <br /><br />
+
+      <button onClick={handleRegister}>Register</button>
 
       <br /><br />
 
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        onChange={handleChange}
-      />
-
-      <br /><br />
-
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        onChange={handleChange}
-      />
-
-      <br /><br />
-
-      <button onClick={handleRegister}>
-        Register
-      </button>
+      <button onClick={() => navigate("/")}>Go to Login</button>
     </div>
   );
 }
